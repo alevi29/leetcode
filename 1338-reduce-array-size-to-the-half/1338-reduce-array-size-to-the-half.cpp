9eeceb22d5814518663fc1776f446cd9f59ee069
alevi29 <109936678+alevi29@users.noreply.github.com>
2022-12-14 00:58:09 -0800

    Time: 224 ms (89.33%), Space: 81.1 MB (30.44%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    typedef pair <int, int> pr;
+public:
+    int minSetSize(vector<int>& arr) {
+        priority_queue <pr> pq;
+        unordered_map <int, int> map;
+        for (int i = 0; i < arr.size(); ++i) {
+            map[arr[i]]++;
+        }
+        for (auto i : map) {
+            pq.push(pr(i.second, i.first));
+        }
+        int count = 0, cur = 0, lim = arr.size() / 2;
+        while (cur < lim) {
+            count++;            
+            cur += pq.top().first;
+            pq.pop();
+        }
+        return count;
+    }
+};
