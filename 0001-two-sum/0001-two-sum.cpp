561da92277b6bd7d3834930fa9010c3df7c1a3b6
alevi29 <109936678+alevi29@users.noreply.github.com>
2023-01-13 12:12:53 -0800

    Time: 12 ms (88.50%), Space: 10.9 MB (30.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,12 +4,9 @@ public:
         unordered_map <int, int> map;
         for (int i = 0; i < nums.size(); ++i) {
             if (map.find(target - nums[i]) != map.end()) {
-                vector <int> ret;
-                ret.push_back(i);
                 for (int j = 0; j < nums.size(); ++j) {
                     if (nums[j] == target - nums[i]) {
-                        ret.push_back(j);
-                        return ret;
+                        return {i, j};
                     }
                 }
             }
