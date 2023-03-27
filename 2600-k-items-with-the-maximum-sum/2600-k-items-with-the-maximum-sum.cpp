204b2305bcb254c54989790d151147c71c7c164f
alevi29 <109936678+alevi29@users.noreply.github.com>
2023-03-26 21:03:08 -0700

    Time: 8 ms (44.44%), Space: 5.9 MB (88.89%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -3,8 +3,6 @@ public:
     int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
         if (k < numOnes) return k;
         else if (numOnes + numZeros > k) return numOnes;
-        else {
-            return numOnes + ((numOnes + numZeros) - k);
-        }
+        else return numOnes + ((numOnes + numZeros) - k);
     }
 };
