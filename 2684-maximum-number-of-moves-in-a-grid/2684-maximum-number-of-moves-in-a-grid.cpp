23ec750c1726fa5e8860d36136993fdcb09a3b4a
alevi29 <109936678+alevi29@users.noreply.github.com>
2023-06-17 13:45:58 -0700

    Time: 247 ms (68.58%), Space: 62.4 MB (93.54%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,17 +1,23 @@
-class Solution {
+
+
+class Solution {    
+
 public:
-    int dfs(vector<vector<int>>& grid, int r, int c, int prev){
-        if(r < 0 || r == grid.size() || c == grid[0].size() || prev >= grid[r][c] ) return 0;
-        prev = grid[r][c];
-        grid[r][c] = INT_MAX;
-        int res = dfs(grid, r-1, c+1, prev);
-        res = max(res, dfs(grid, r, c+1, prev ));
-        res = max(res, dfs(grid, r+1, c+1, prev));
-        return res + 1;
+    int search(int i, int j, vector<vector<int>>& theGrid, int prev) {
+        if (i < 0 || i == theGrid.size() || j >= theGrid[0].size() || theGrid[i][j] <= prev) return 0;
+        prev = theGrid[i][j];
+        theGrid[i][j] = INT_MAX;
+        int ret = search(i - 1, j + 1, theGrid, prev);
+        ret = max(ret, search(i, j + 1, theGrid, prev));
+        ret = max(ret, search(i + 1, j + 1, theGrid, prev));
+        return ret + 1;
     }
+    
     int maxMoves(vector<vector<int>>& grid) {
-        int ans = 0;
-        for(int i = 0; i < grid.size(); ++i) ans = max(ans, dfs(grid, i, 0, 0));
-        return ans-1;
+        int ret = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            ret = max(ret, search(i, 0, grid, 0));
+        }
+        return ret - 1;
     }
 };
