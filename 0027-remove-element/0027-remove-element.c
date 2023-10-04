c62a4deb5431b58f0c56c3567c2301362db3cf5d
Alex Cai <109936678+alevi29@users.noreply.github.com>
2023-10-04 14:11:35 -0700

    Time: 4 ms (49.50%), Space: 6.7 MB (10.62%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,12 @@
+
+
+int removeElement(int* nums, int numsSize, int val){
+    int index = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+}
